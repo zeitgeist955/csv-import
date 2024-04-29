7853ae4df932b426db5b319879b3d46de0b3e9ab
Nicolas Deman <nicolas.deman@hotmail.fr>
2024-04-29 14:32:19 +0200

    spring initialz

---
+++ src/main/java/com/zg955/csvimport/CsvImportApplication.java
@@ -0,0 +1,13 @@
+package com.zg955.csvimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvImportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zg955/csvimport/CsvImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zg955.csvimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvImportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
